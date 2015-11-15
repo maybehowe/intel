fa675e9cd90cebc340d927c40819e090f5aec0a8
Howe <hhhaohao@163.com>
2015-11-15 15:56:01 +0800

    howe page over

---
+++ js/howe.js
@@ -0,0 +1,9 @@
+(function(){
+	window.intel = {
+		over_link: ['http://v.youku.com/v_show/id_XMTM0ODU1NDI4OA==.html',
+					'http://v.youku.com/v_show/id_XMTM0ODU1ODI1Mg==.html',
+					'http://v.youku.com/v_show/id_XMTM0ODU1ODcwNA==.html',
+					'http://v.youku.com/v_show/id_XMTM0ODU1OTg0NA==.html'],
+		one_line: 'http://prcappzone.intel.com/it-management/it-future-phone/index.html?utm_source=Social&utm_medium=h5&utm_campaign=biz_pc_preference_campaign',
+	}
+})();
